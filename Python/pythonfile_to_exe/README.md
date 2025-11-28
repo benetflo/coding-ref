6ccee608a0a2ea69134c8dbaa4b8c643106718e1
benflo <ben.flodin@outlook.com>
2025-11-28 15:05:03 +0100

    added python to exe and windows toast examples

---
+++ Python/pythonfile_to_exe/README.md
@@ -0,0 +1,7 @@
+# Usage
+
+```
+pyinstaller --onefile main.py
+```
+
+- Executable will be found in the dist/ folder.
--- windows/notification_system/main.py
+++ windows/notification_system/main.py
@@ -1,8 +1,11 @@
-from win10toast import ToastNotifier
+from windows_toasts import Toast, WindowsToaster, AudioSource, ToastAudio
 
-toaster = ToastNotifier()
+toaster = WindowsToaster('!!! YOU WON CONGRATULATIONS !!!')
 
-toaster.show_toast("You got mail!",
-                   "Contents of mail...",
-                   icon_path="custom.ico",
-                   duration=10)
+newToast = Toast()
+newToast.text_fields = ['Hello there! You just won a thousand dollars! Click me to claim it!']
+newToast.launch_action = 'https://www.youtube.com/watch?v=XfELJU1mRMg'
+newToast.audio = ToastAudio(AudioSource.IM, looping=True)
+
+# send notification
+toaster.show_toast(newToast)
